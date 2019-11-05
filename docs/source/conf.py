ae64d792964883bb64eff5520ab1981e10653293
Ang Li <leon575777642@gmail.com>
2019-11-05 13:56:15 -0500

    Updates on .readthedocs.yaml and docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Master document containing the root toctree
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 
