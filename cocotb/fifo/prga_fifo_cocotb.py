9784296f2ad8c9377e73e5b5d7df3e15eb5e6796
crusader2000 <ansh.puvvada@students.iiit.ac.in>
2020-05-09 16:55:58 +0530

    Identation Changes

--- cocotb/fifo/prga_fifo_cocotb.py
+++ cocotb/fifo/prga_fifo_cocotb.py
@@ -23,59 +23,59 @@ def always_block(dut):
 		dut.C_rd <= 0
 		dut.D_rd <= 0
 	else:
-        if(dut.A_full.value==0 and dut.A_rd_cnt == len(src)):
-            dut.A_wr_cnt <= (int(dut.A_wr_cnt.value) + 1)
+		if(dut.A_full.value==0 and dut.A_rd_cnt == len(src)):
+			dut.A_wr_cnt <= (int(dut.A_wr_cnt.value) + 1)
 
-        if (!dut.B_full.value and dut.B_rd_cnt == len(src)):
-            dut.B_wr_cnt <= dut.B_wr_cnt + 1
+		if (!dut.B_full.value and dut.B_rd_cnt == len(src)):
+			dut.B_wr_cnt <= dut.B_wr_cnt + 1
+		
+		if (!dut.C_full.value and dut.C_rd_cnt == len(src)):
+			dut.C_wr_cnt <= dut.C_wr_cnt + 1
 
-        if (!dut.C_full.value and dut.C_rd_cnt == len(src)):
-            dut.C_wr_cnt <= dut.C_wr_cnt + 1
+		if (!dut.D_full.value and dut.D_rd_cnt == len(src)):
+			dut.D_wr_cnt <= dut.D_wr_cnt + 1
 
-        if (!dut.D_full.value and dut.D_rd_cnt == len(src)):
-            dut.D_wr_cnt <= dut.D_wr_cnt + 1
+		dut.A_valid <= !dut.A_empty and dut.A_rd
 
-        dut.A_valid <= !dut.A_empty and dut.A_rd
-        
-        if (dut.A_valid.value):
-            if (src[dut.A_rd_cnt] != dut.A_dout):
-                error <= 1
-                # $display("[ERROR] A output No. %d 0x%08x != 0x%08x", A_rd_cnt, A_dout, src[A_rd_cnt])
-            dut.A_rd_cnt <= dut.A_rd_cnt + 1
+		if (dut.A_valid.value):
+			if (src[dut.A_rd_cnt] != dut.A_dout):
+				error <= 1
+				# $display("[ERROR] A output No. %d 0x%08x != 0x%08x", A_rd_cnt, A_dout, src[A_rd_cnt])
+			dut.A_rd_cnt <= dut.A_rd_cnt + 1
 
-        dut.B_valid <= !dut.B_empty and dut.B_rd
-    
-        if (dut.B_valid.value):
-            if (src[dut.B_rd_cnt] != dut.B_dout):
-                error <= 1
-                # $display("[ERROR] B output No. %d 0x%08x != 0x%08x", B_rd_cnt, B_dout, src[B_rd_cnt])
-            dut.B_rd_cnt <= dut.B_rd_cnt + 1
+		dut.B_valid <= !dut.B_empty and dut.B_rd
 
-        if (!dut.C_empty.value and dut.C_rd):
-            if (src[dut.C_rd_cnt] != dut.C_dout):
-                error <= 1
-                # $display("[ERROR] C output No. %d 0x%08x != 0x%08x", C_rd_cnt, C_dout, src[C_rd_cnt])
-            dut.C_rd_cnt <= dut.C_rd_cnt + 1
+		if (dut.B_valid.value):
+			if (src[dut.B_rd_cnt] != dut.B_dout):
+				error <= 1
+				# $display("[ERROR] B output No. %d 0x%08x != 0x%08x", B_rd_cnt, B_dout, src[B_rd_cnt])
+			dut.B_rd_cnt <= dut.B_rd_cnt + 1
 
-        if (!dut.D_empty.value and dut.D_rd):
-            if (src[dut.D_rd_cnt] != dut.D_dout):
-                error <= 1
-                # $display("[ERROR] D output No. %d 0x%08x != 0x%08x", D_rd_cnt, D_dout, src[D_rd_cnt])
-            D_rd_cnt <= D_rd_cnt + 1
+		if (!dut.C_empty.value and dut.C_rd):
+			if (src[dut.C_rd_cnt] != dut.C_dout):
+				error <= 1
+				# $display("[ERROR] C output No. %d 0x%08x != 0x%08x", C_rd_cnt, C_dout, src[C_rd_cnt])
+			dut.C_rd_cnt <= dut.C_rd_cnt + 1
 
-        dut.A_rd <= randint(0,2) == 0
-        dut.B_rd <= randint(0,2) == 0
-        dut.C_rd <= randint(0,2) == 0
-        dut.D_rd <= randint(0,2) == 0
+		if (!dut.D_empty.value and dut.D_rd):
+			if (src[dut.D_rd_cnt] != dut.D_dout):
+				error <= 1
+				# $display("[ERROR] D output No. %d 0x%08x != 0x%08x", D_rd_cnt, D_dout, src[D_rd_cnt])
+			D_rd_cnt <= D_rd_cnt + 1
 
-        if (dut.A_rd_cnt == len(src) and
-            dut.B_rd_cnt == len(src) and
-            dut.C_rd_cnt == len(src) and
-            dut.D_rd_cnt == len(src))
-            if (error):
-                print("[FAIL]")
-            else:
-                print("[PASS]")
+		dut.A_rd <= randint(0,2) == 0
+		dut.B_rd <= randint(0,2) == 0
+		dut.C_rd <= randint(0,2) == 0
+		dut.D_rd <= randint(0,2) == 0
+
+		if (dut.A_rd_cnt == len(src) and
+			dut.B_rd_cnt == len(src) and
+			dut.C_rd_cnt == len(src) and
+			dut.D_rd_cnt == len(src))
+			if (error):
+				print("[FAIL]")
+			else:
+				print("[PASS]")
 
 async def reset_signal(dut):
 	dut.rst <= 0
