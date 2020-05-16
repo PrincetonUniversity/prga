0a7160798f57e35dae7d4efdbfa1e0638e9b87c3
Ansh Puvvada <43775333+crusader2000@users.noreply.github.com>
2020-05-16 09:39:52 +0530

    Update fifo_tb.v

--- cocotb/fifo_non_lookahead/fifo_tb.v
+++ cocotb/fifo_non_lookahead/fifo_tb.v
@@ -1,27 +1,3 @@
-`timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   08:36:39 05/16/2020
-// Design Name:   fifo
-// Module Name:   C:/Users/ANSH/gsoc/fifo_tb.v
-// Project Name:  gsoc
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: fifo
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module fifo_tb;
    localparam DATA_WIDTH = 8;
 	// Inputs
