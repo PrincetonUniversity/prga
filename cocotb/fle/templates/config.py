801a08adb976cb515ac0c2157ad9fe6819f5f3f2
crusader2000 <ansh.puvvada@students.iiit.ac.in>
2020-07-22 19:20:05 +0530

    Removed unnecessary folder

--- cocotb/fle/templates/config.py
+++
@@ -1,3 +0,0 @@
-internal_ff = [0,0]
-internal_lut = [0,0]
-internal_sum = 0
--- cocotb/lut/build.py
+++
@@ -1,8 +0,0 @@
-from prga.core.common import ModuleView
-from prga.cfg.scanchain.lib import Scanchain
-from prga.passes.rtl import VerilogCollection
-ctx = Scanchain.new_context(1)
-r = Scanchain.new_renderer()
-p = VerilogCollection(r, "rtl")
-p._process_module(ctx.database[ModuleView.logical, "lut4"])
-r.render()
