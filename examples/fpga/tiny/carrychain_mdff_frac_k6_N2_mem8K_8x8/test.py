585cbebe24f773f05c78517807988d964d9b2ca0
crusader2000 <ansh.puvvada@students.iiit.ac.in>
2020-08-18 11:09:26 +0530

    Updated the test build files for examples

--- examples/fpga/tiny/carrychain_mdff_frac_k6_N2_mem8K_8x8/test.py
+++ examples/fpga/tiny/carrychain_mdff_frac_k6_N2_mem8K_8x8/test.py
@@ -1,11 +1,6 @@
 from prga import *
 from prga.core.context import *
-from prga.netlist.net.util import NetUtils
-from prga.compatible import *
-from itertools import chain
-from bitarray import bitarray
 from prga.passes.test import Tester
-from prga.core.builder.box.sbox import _SwitchBoxKey
 ctx = Context.unpickle("ctx.pkl")
 
 flow = Flow(
---
+++ examples/fpga/tiny/mdff_frac_k6_N2_mem8K_8x8/test.py
@@ -0,0 +1,10 @@
+from prga import *
+from prga.core.context import *
+from prga.passes.test import Tester
+ctx = Context.unpickle("ctx.pkl")
+
+
+flow = Flow(
+        Tester('rtl','unit_tests')
+        )
+flow.run(ctx, Scanchain.new_renderer())
